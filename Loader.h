e72659dea0d3e0cb0880171dc4ce60d989d3a5a1
garhogrebe <garhogrebe@gmail.com>
2021-08-07 16:12:28 -0700

    Loader class files added

---
+++ Loader.h
@@ -0,0 +1,38 @@
+#ifndef AL_Loader_H
+#define AL_Loader_H
+#include "AdvancedLED4.h"
+
+//Variable loader class for loading animations
+class Loader {
+public:
+	//How many variables are loaded into the array right now
+	int num_loaded = 0;
+	//Array for holding the potential variables to load. May change to not be array at some point
+	int variables[1000];
+
+	//Clears the Loader class to prepare for new load
+	void clearLoader();
+
+	//ToDo appends a variable to the loader class
+	void append(int new_int);
+
+	//ToDo fills in the loader class from a string
+	void loadByString();
+
+
+};
+
+extern Loader main_loader;
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
